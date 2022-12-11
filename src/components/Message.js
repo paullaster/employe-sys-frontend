f74d0859ad0c2c3404938fedbffc801e0c6b032b
paullaster <paullasterokoth98@gmail.com>
2022-12-11 17:59:39 +0300

    contruct message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,13 @@
-import FlassMessage from 'react-flash-message';
+import FlassMessage from "react-flash-message";
 
-const Message = ({duration, persistOnHover='true'}) => {
-    return ();
+const Message = ({ duration, persistOnHover = "true", message }) => {
+  return (
+    <FlassMessage duration={duration} persistOnHover={persistOnHover}>
+      <p>
+        {message}
+      </p>
+    </FlassMessage>
+  );
 };
 
 export default Message;
