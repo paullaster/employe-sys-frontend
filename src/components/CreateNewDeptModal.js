0224b1784e1608aa558e56c8bb04be6b806ebe2d
paullaster <paullasterokoth98@gmail.com>
2022-12-11 14:50:27 +0300

    create new dept

---
+++ src/components/CreateNewDeptModal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Form from "./Form";
+import LabelInput from "./LabelIpunt";
+import Button from "./Button";
+import '../assets/model.css'
+const CreateNewDeptModal = ({deptId, deptName, deptManager, closeModel, createNewDept  }) => {
+            return (
+                <div className="modal">
+                  <div className="modal-container-dept_update">
+                    <div className="modal-header">
+                      <div className="closeModel" onClick={closeModel}>&times;</div>
+                    </div>
+                    <div className="modal-body">
+                      <Form>
+                        <LabelInput handleChange={deptId}>Department ID</LabelInput>
+                        <LabelInput handleChange={deptName}>Department Name</LabelInput>
+                        <LabelInput handleChange={deptManager}>Department Manager</LabelInput>
+                       
+                      </Form>
+                    </div>
+                    <div className="modal-footer">
+                        <Button onClick={()=> {closeModel (); createNewDept ();}}>Update</Button>
+                    </div>
+                  </div>
+                </div>
+                )
+}
+export default CreateNewDeptModal;
